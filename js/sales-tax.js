7cf930df4636fc486108abaeadfea054e78752d6
Thomas Crowder <thomas.levi.crowder3@gmail.com>
2020-08-05 12:29:42 -0500

    worked on sales-tax exercise

--- js/sales-tax.js
+++ js/sales-tax.js
@@ -1,1 +1,44 @@
 "use strict"
+
+var shopperBook = prompt("How many books would you like to buy?");
+var shopperMusic = prompt("How many music CD's would you like to buy?");
+var shopperChocolate = prompt("How much chocolate would you like to buy?");
+var tax = 0.10
+
+
+var items = [
+    {
+        amount: shopperBook,
+        name: "Book",
+        class: "Books",
+        price: 12.49
+    },
+    {
+        amount: shopperMusic,
+        name: "Music CD",
+        class: "Recreation",
+        price: 14.99
+    },
+    {
+        amount: shopperChocolate,
+        name: "Chocolate Bar",
+        class:"Food",
+        price: 0.85
+    }
+
+];
+
+items.forEach(function (item) {
+    var taxPrice = (item.price * tax)+item.price;
+    if (item.class === "Books" || item.class === "Food" || item.class === "Medicine"){
+        console.log(item.amount + " " + item.name + " " + (item.price * item.amount));
+    } else{
+        console.log(item.amount + " " + item.name + " " + (taxPrice * item.amount));
+    }
+    if (item.class !== "Books" || item.class !== "Food"|| item.class !== "Medicine"){
+        console.log(taxPrice-item.price)
+    }else{
+
+    }
+})
+
