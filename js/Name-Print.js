bdcff0ef3f9e4a9d30d98a257fd1e1f70eb9a1d9
Thomas Crowder <thomas.levi.crowder3@gmail.com>
2020-08-08 00:45:16 -0500

    worked on personal practice

--- js/Name-Print.js
+++ js/Name-Print.js
@@ -1,11 +1,21 @@
 (function() {
 "use strict"
 
+var userName = prompt("What is your name?")
 
+var peopleNames = ["Jeffery","Steven","Suzy","Sean","Shaun","Anthony",userName]
+function guessName(name){
+   for(name = 0; name < peopleNames.length;name++) {
+       if (name === peopleNames.indexOf(userName)){
+           console.log(peopleNames[name] + ": This is your name")
+       } else {
+           console.log(peopleNames[name] + ": This is not your name")
+       }
+   }
 
+}
 
-
-
+    console.log(guessName());
 
 
 }());
