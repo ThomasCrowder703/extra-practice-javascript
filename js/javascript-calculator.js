62dd3235c629aad62ae2ae12c03637a554401555
Thomas Crowder <thomas.levi.crowder3@gmail.com>
2020-09-06 23:56:30 -0500

    experimented with functions for calculator

--- js/javascript-calculator.js
+++ js/javascript-calculator.js
@@ -1,1 +1,28 @@
-console.log("ready to go");
+"use strict"
+
+var values = function (x) {
+    let html = "<div>"
+    html += "it works"
+
+    html += "</div>"
+    return html
+}
+
+
+document.getElementById('value-buttons').addEventListener('click', values);
+
+var addition = function add(x,y) {
+    return x + y;
+}
+
+var subtraction = function sub(x,y) {
+    return x - y;
+}
+
+var multiplication = function mult(x,y) {
+    return x * y
+}
+
+var division = function divi(x,y) {
+    return x / y;
+}
