4c389bf1449cd0d988ecc16713a1f427e834d589
Thomas Crowder <thomas.levi.crowder3@gmail.com>
2020-09-18 01:08:50 -0500

    feat: schedule -- took the array approach to user request

--- js/schedule.js
+++ js/schedule.js
@@ -1,12 +1,19 @@
 const weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday","Saturday","Sunday"]
+let requestedDays = []
+
+$("#userSubmit").click(function () {
+    console.log($("#userRequest").val())
+})
+
+    for(var i = 0; i <= 1; i++){
+        requestedDays.push($("#userRequest").val())
+    }
+    console.log(requestedDays)
+
 
 function determineWork(x) {
     for(var i = 0; i < weekdays.length; i++){
-        if(weekdays[i] === "Saturday" || weekdays[i] === "Sunday"){
-            console.log("You do not work on " + weekdays[i])
-        }else{
-            console.log("You work on " + weekdays[i])
-        }
+
     }
 }
 
