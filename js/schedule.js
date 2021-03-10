2c88b71aa70492e18caa4eba1941d27723569322
Thomas Crowder <thomas.levi.crowder3@gmail.com>
2021-03-10 10:11:52 -0600

    basic function

--- js/schedule.js
+++ js/schedule.js
@@ -1,26 +1,31 @@
-const weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday","Saturday","Sunday"];
-let requestedDays = [];
+(function () {
+// const weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday","Saturday","Sunday"];
+// let requestedDays = [];
+//
+// $("#userSubmit").click(function () {
+//    requestedDays.push($("#userRequest").val())
+//     console.log(requestedDays);
+//    let userConfirm = confirm("Are you sure this is the day you wan to take off?");
+//    if(userConfirm === true){
+//        $("#userSubmit").hide();
+//        let newSchedule =[];
+//        for(let i =0; i< weekdays.length; i++){
+//            if(weekdays[i] !== requestedDays[0]){
+//                newSchedule.push(weekdays[i]);
+//            }
+//        }
+//        console.log(newSchedule);
+//    }else{
+//        requestedDays = [];
+//    }
+//
+//
+// })
+for(let i =0; i<=10; i++){
+    console.log(i)
+}
 
-$("#userSubmit").click(function () {
-   requestedDays.push($("#userRequest").val())
-    console.log(requestedDays);
-   let userConfirm = confirm("Are you sure this is the day you wan to take off?");
-   if(userConfirm === true){
-       $("#userSubmit").hide();
-       let newSchedule =[];
-       for(let i =0; i< weekdays.length; i++){
-           if(weekdays[i] !== requestedDays[0]){
-               newSchedule.push(weekdays[i]);
-           }
-       }
-       console.log(newSchedule);
-   }else{
-       requestedDays = [];
-   }
-
-
-})
-
+})();
 
 
 
