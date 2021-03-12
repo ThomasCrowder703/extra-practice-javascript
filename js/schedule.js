9852a20bc935de6d5fdcddee52a377e852bea93c
Thomas Crowder <thomas.levi.crowder3@gmail.com>
2021-03-12 10:03:31 -0600

    image shakes on hover

--- js/schedule.js
+++ js/schedule.js
@@ -1,29 +1,8 @@
 (function () {
-// const weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday","Saturday","Sunday"];
-// let requestedDays = [];
-//
-// $("#userSubmit").click(function () {
-//    requestedDays.push($("#userRequest").val())
-//     console.log(requestedDays);
-//    let userConfirm = confirm("Are you sure this is the day you wan to take off?");
-//    if(userConfirm === true){
-//        $("#userSubmit").hide();
-//        let newSchedule =[];
-//        for(let i =0; i< weekdays.length; i++){
-//            if(weekdays[i] !== requestedDays[0]){
-//                newSchedule.push(weekdays[i]);
-//            }
-//        }
-//        console.log(newSchedule);
-//    }else{
-//        requestedDays = [];
-//    }
-//
-//
-// })
-for(let i =0; i<=10; i++){
-    console.log(i)
-}
+
+$(".minus-image").hover(function () {
+
+})
 
 })();
 
